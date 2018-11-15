8337515fb9e83bff0f41f66bcc59198ab5ff16f6
ouki-wang <358023925@qq.com>
2018-11-15 17:24:31 +0800

    add raspberry

---
+++ RaspberryPi/Python/README.md
@@ -0,0 +1,82 @@
+## DFRobot_AS3935_Lib.py Library for Raspberry pi
+---------------------------------------------------------
+This is the sample code for Gravity:Lightning Sensor, SKU: SEN0292.
+## Table of Contents
+
+* [Installation](#installation)
+* [Methods](#methods)
+<snippet>
+<content>
+
+## Installation
+The Lightning Sensor should work with AS3935
+(https://github.com/DFRobot/DFRobot_AS3935/tree/master/RaspberryPi/Python) 
+
+Run the program:
+
+```cpp
+
+$> python DFRobot_AS3935.py
+
+```
+## Methods
+
+```C++
+
+/*
+ * @brief Init The Lightning Sensor
+ *
+ * @param address  I2C address(1~3)
+ *        bus      I2C bus
+ */
+DFRobot_AS3935(address, bus);
+
+/*
+ * @brief Sensor reset
+ */
+def reset(self);
+
+/*
+ * @brief Configure sensor
+ *
+ * @param capacitance Antenna tuning capcitance (must be integer multiple of 8, 8 - 120 pf)
+ *        location    Indoor/outdoor mode selection
+ *        disturber   Enable/disable disturber detection
+ */
+def manualCal(self, capacitance, location, disturber);
+
+/*
+ * @brief Get mid-range type
+ *
+ * @return 0  Unknown src
+ *         1  Lightning detected
+ *         2  Disturber
+ *         3  Noise level too high
+ */
+def getInterruptSrc(self);
+
+/*
+ * @brief get lightning distance
+ * 
+ * @return unit kilometer
+ */
+def getLightningDistKm(self);
+
+/*
+ * @brief get lightning energy intensity
+ * 
+ * @return lightning energy intensity(0-1000)
+ */
+def getStrikeEnergyRaw(self);
+
+```
+## Credits
+
+Written by DFRobot_JH, 2018. (Welcome to our [website](https://www.dfrobot.com/))
+
+
+
+
+
+
+
---
+++ RaspberryPi/Python/example/DFRobot_AS3935.py
@@ -0,0 +1,82 @@
+# file DFRobot_AS3935_lightning_sensor.py
+#
+# SEN0290 Lightning Sensor
+# This sensor can detect lightning and display the distance and intensity of the lightning within 40 km
+# It can be set as indoor or outdoor mode.
+# The module has three I2C, these addresses are:
+# AS3935_ADD1  0x01   A0 = High  A1 = Low
+# AS3935_ADD3  0x03   A0 = High  A1 = High
+# AS3935_ADD2  0x02   A0 = Low   A1 = High
+#
+#
+# Copyright    [DFRobot](http://www.dfrobot.com), 2018
+# Copyright    GNU Lesser General Public License
+#
+# version  V0.1
+# date  2018-11-13
+
+import time
+from DFRobot_AS3935_Lib import DFRobot_AS3935
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+#I2C address
+AS3935_I2C_ADDR1 = 0X01
+AS3935_I2C_ADDR2 = 0X02
+AS3935_I2C_ADDR3 = 0X03
+
+#Antenna tuning capcitance (must be integer multiple of 8, 8 - 120 pf)
+AS3935_CAPACITANCE = 96
+IRQ_PIN = 37
+
+#Indoor/outdoor mode selection
+AS3935_INDOORS = 0
+AS3935_OUTDOORS = 1
+AS3935_MODE = AS3935_INDOORS
+
+#Enable/disable disturber detection
+AS3935_DIST_DIS = 0
+AS3935_DIST_EN = 1
+AS3935_DIST = AS3935_DIST_EN
+
+GPIO.setmode(GPIO.BOARD)
+
+sensor = DFRobot_AS3935(AS3935_I2C_ADDR3, bus = 1)
+sensor.reset()
+#Configure sensor
+sensor.manualCal(AS3935_CAPACITANCE, AS3935_MODE, AS3935_DIST)
+
+# Connect the IRQ and GND pin to the oscilloscope.
+# uncomment the following sentences to fine tune the antenna for better performance.
+# This will dispaly the antenna's resonance frequency/16 on IRQ pin (The resonance frequency will be divided by 16 on this pin)
+# Tuning AS3935_CAPACITANCE to make the frequency within 500/16 kHz ± 3.5%
+#
+# sensor.setLcoFdiv(0)
+# sensor.setIrqOutputSource(3)
+
+def callback_handle(channel):
+    global sensor
+    time.sleep(0.005)
+    intSrc = sensor.getInterruptSrc()
+    if intSrc == 1:
+        lightningDistKm = sensor.getLightningDistKm()
+        print('Lightning occurs!')
+        print('Distance: %dkm'%lightningDistKm)
+
+        lightningEnergyVal = sensor.getStrikeEnergyRaw()
+        print('Intensity: %d '%lightningEnergyVal)
+    elif intSrc == 2:
+        print('Disturber discovered!')
+    elif intSrc == 3:
+        print('Noise level too high!')
+    else:
+        pass
+#Set to input mode
+GPIO.setup(IRQ_PIN, GPIO.IN)
+#Set the interrupt pin, the interrupt function, rising along the trigger
+GPIO.add_event_detect(IRQ_PIN, GPIO.RISING, callback = callback_handle)
+
+while True:
+    time.sleep(1.0)
+
+
