fe7f222abb0977c3bf5146973b5aff6779f66794
ouki-wang <358023925@qq.com>
2018-11-15 11:07:27 +0800

    V0.4

--- DFRobot_AS3935_I2C.h
+++ DFRobot_AS3935_I2C.h
@@ -15,45 +15,45 @@
 class DFRobot_AS3935_I2C
 {
  public:
-	DFRobot_AS3935_I2C(uint8_t irqx, uint8_t devAddx);
-	/*! Set i2c address */
-	void setI2CAddress(uint8_t devAddx);
-	/*! Manual calibration */
-	void manualCal(uint8_t capacitance, uint8_t location, uint8_t disturber);
-	/*! reset registers to default */
-	void defInit(void);
-	void powerUp(void);
-	void powerDown(void);
-	void disturberEn(void);
-	void disturberDis(void);
-	void setIRQOutputSource(uint8_t irqSelect);
-	void setTuningCaps(uint8_t capVal);
-	/*! 0 = unknown src, 1 = lightning detected, 2 = disturber, 3 = Noise level too high */
-	uint8_t getInterruptSrc(void);
-	/*! Get rid of non-distance data */
-	uint8_t getLightningDistKm(void);
-	/*! Get lightning energy intensity */
-	uint32_t getStrikeEnergyRaw(void);
-	uint8_t setMinStrikes(uint8_t minStrk);
-	void clearStatistics(void);
-	void setIndoors(void);
-	void setOutdoors(void);
-	uint8_t getNoiseFloorLvl(void);
-	void setNoiseFloorLvl(uint8_t nfSel);
-	uint8_t getWatchdogThreshold(void);
-	void setWatchdogThreshold(uint8_t wdth);
-	uint8_t getSpikeRejection(void);
-	void setSpikeRejection(uint8_t srej);
-	void setLcoFdiv(uint8_t fdiv);
-	/*! View register data */
-	void printAllRegs(void);
-	
+    DFRobot_AS3935_I2C(uint8_t irqx, uint8_t devAddx);
+    /*! Set i2c address */
+    void setI2CAddress(uint8_t devAddx);
+    /*! Manual calibration */
+    void manualCal(uint8_t capacitance, uint8_t location, uint8_t disturber);
+    /*! reset registers to default */
+    void defInit(void);
+    void powerUp(void);
+    void powerDown(void);
+    void disturberEn(void);
+    void disturberDis(void);
+    void setIRQOutputSource(uint8_t irqSelect);
+    void setTuningCaps(uint8_t capVal);
+    /*! 0 = unknown src, 1 = lightning detected, 2 = disturber, 3 = Noise level too high */
+    uint8_t getInterruptSrc(void);
+    /*! Get rid of non-distance data */
+    uint8_t getLightningDistKm(void);
+    /*! Get lightning energy intensity */
+    uint32_t getStrikeEnergyRaw(void);
+    uint8_t setMinStrikes(uint8_t minStrk);
+    void clearStatistics(void);
+    void setIndoors(void);
+    void setOutdoors(void);
+    uint8_t getNoiseFloorLvl(void);
+    void setNoiseFloorLvl(uint8_t nfSel);
+    uint8_t getWatchdogThreshold(void);
+    void setWatchdogThreshold(uint8_t wdth);
+    uint8_t getSpikeRejection(void);
+    void setSpikeRejection(uint8_t srej);
+    void setLcoFdiv(uint8_t fdiv);
+    /*! View register data */
+    void printAllRegs(void);
+    
  private:
-	uint8_t irq, devAdd;
-	uint8_t singRegRead(uint8_t regAdd);
-	void singRegWrite(uint8_t regAdd, uint8_t dataMask, uint8_t regData);
-	void AS3935Reset(void);
-	void calRCO(void);
+    uint8_t irq, devAdd;
+    uint8_t singRegRead(uint8_t regAdd);
+    void singRegWrite(uint8_t regAdd, uint8_t dataMask, uint8_t regData);
+    void AS3935Reset(void);
+    void calRCO(void);
 };
 
 #endif
