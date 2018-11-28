38f851e63d1cf454ae68419d53809780f0c8d0c7
ouki-wang <358023925@qq.com>
2018-11-28 17:01:47 +0800

    V1.0

--- RaspberryPi/Python/DFRobot_AS3935_Lib.py
+++ RaspberryPi/Python/DFRobot_AS3935_Lib.py
@@ -194,7 +194,7 @@ class DFRobot_AS3935:
         #This function is used to modify WDTH. It is used to increase robustness to disturbers,
         #though will make detection less efficient (see page 19, Fig 20 of datasheet)
         #WDTH register: add 0x01, bits 3:0
-        #default value of 0001
+        #default value of 0010
         #values should only be between 0x00 and 0x0F (0 and 7)
         self.singRegWrite(0x01, 0x0F, wdth & 0x0F)
 
